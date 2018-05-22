d7b4f7efd15b50fbd31acb7275e5406f3d4c696f
sadarukun <pnt.so.krn22@gmail.com>
2018-05-22 15:47:21 +0900

    最新

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,25 @@
 var module = angular.module('my-app', ['onsen'])
 module.controller('AppController', function ($scope) {
-	this.load = function (page) {
-		splitter.content.load(page)
-			.then(function () {
-				splitter.left.close()
-                    if(page == 'home.html'){
+     this.load = function (page) {
+          splitter.content.load(page)
+               .then(function () {
+                    splitter.left.close()
+                    if (page == 'home.html') {
                          initialize()
                     }
-			})
-	}
+               })
+     }
 });
 
- var contentString = '梅田さんの家らしい';
+var contentString = "<div>"+
+"<h1 class='firstHeading'>扇町公園のトイレ</h1>"+
+"<img src='img/test.jpg' class='img'></img>"+
+"<div class='info'>評価:★★★<br>"+
+"清潔度:★★★<br>"+
+"営業時間:年中無休<br>"+
+"<input type='button' value='詳細' onClick='detail()'></div></div>"
 
-  function initialize() {
+function initialize() {
      var mapOptions = {
           center: new google.maps.LatLng(34.703615, 135.509339),    //地図上で表示させる緯度経度
           zoom: 14,                                                 //地図の倍率
@@ -21,19 +27,41 @@ module.controller('AppController', function ($scope) {
      };
      var map = new google.maps.Map(document.getElementById("map_canvas"),
           mapOptions);
-          var myLatlng = new google.maps.LatLng(34.703615, 135.509339); //扇町公園の経度、緯度
-          var marker = new google.maps.Marker({
+
+     var simpleMapStyle, map;
+
+     // POI を非表示にするマップタイプを定義
+     simpleMapStyle = new google.maps.StyledMapType([
+          {
+               featureType: "poi",
+               elementType: "labels",
+               stylers: [
+                    { visibility: "off" }
+               ]
+          }
+     ], { name: "Simple Map" });
+
+     // マップタイプを追加して設定
+     map.mapTypes.set("simple_map", simpleMapStyle);
+     map.setMapTypeId("simple_map");
+
+     var myLatlng = new google.maps.LatLng(34.703615, 135.509339); //扇町公園の経度、緯度
+     var marker = new google.maps.Marker({
           position: myLatlng,
-          title:"梅田の家"
+          title: "梅田の家"
      });
      marker.setMap(map);
-     　　//ポップアップ表示のやつ
-     marker.addListener( "click", function ( ) { 
-           var infowindow = new google.maps.InfoWindow({
-               content:contentString
-           });
+     //ポップアップ表示のやつ
+     marker.addListener("click", function () {
+          var infowindow = new google.maps.InfoWindow({
+               content: contentString
+          });
           infowindow.open(map, marker);
-} ) ;
+     });
+}
+
+function detail(){
+     navi.pushPage('detail.html')
 }
 
 //💩
